26b54a62c6dff9bbc68396e998ea0b65d40cf56c
Amir Shacham <shacham.amir@gmail.com>
2026-03-31 09:48:54 +0300

    revised version of basics oop dictionary usage

--- OOP/Basics.py
+++ OOP/Basics.py
@@ -1,19 +1,25 @@
-# Working with dictionaries only
-def greet():
-    print("Hello world!")
+
+# Wroking with dictionaries only
+def drive(speed):
+    print(f"the car is driving at speed {speed} km/h")
 
 car1 = {
     "brand": "Toyota",
     "model": "yaris",
     "year": 2015,
     "tank": 1.6,
+    "max_speed": 120,
     "is_active": True,
-    "say_hello": greet
+    "drive": drive
 }
 
-car1["say_hello"]()
+car1["drive"](car1["max_speed"])
 
 car2 = {
     "brand": "Toyota",
     "model": "yaris",
+    "yaer": 2015,
+    "tank": 1.6,
+    "is_active": True,
+    "gaz": "95"
 }
